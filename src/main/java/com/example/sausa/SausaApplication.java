b9ba29654f7957f9411e3c25a0ee980c04409902
ManSavc <savickas.mantvydas@gmail.com>
2024-01-06 00:47:00 +0200

    Initial commit

---
+++ src/main/java/com/example/sausa/SausaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sausa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SausaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SausaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sausa/SausaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sausa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SausaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
